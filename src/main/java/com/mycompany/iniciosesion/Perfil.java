ac020a299bf1dedaaaccf1d30e732c022fc6eac2
Elissa <Elissa@DESKTOP-MH83FMH>
2025-02-27 17:21:17 -0700

    yaestoyharta

--- src/main/java/com/mycompany/iniciosesion/Perfil.java
+++ src/main/java/com/mycompany/iniciosesion/Perfil.java
@@ -26,19 +26,24 @@ public class Perfil extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jScrollPane1 = new javax.swing.JScrollPane();
         background = new javax.swing.JPanel();
         barramenu = new javax.swing.JPanel();
         inicio = new javax.swing.JButton();
-        menu = new javax.swing.JButton();
         perfil = new javax.swing.JButton();
         amigos = new javax.swing.JButton();
         notificaciones = new javax.swing.JButton();
-        infousuario = new javax.swing.JPanel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        publicaciones = new javax.swing.JPanel();
+        nombreusuario = new javax.swing.JLabel();
+        notperfil = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
+        menu = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(255, 255, 255));
+        setForeground(java.awt.Color.white);
 
-        jScrollPane1.setBackground(new java.awt.Color(255, 255, 255));
+        background.setBackground(new java.awt.Color(255, 255, 255));
 
         barramenu.setBackground(new java.awt.Color(255, 255, 255));
 
@@ -50,17 +55,6 @@ public class Perfil extends javax.swing.JFrame {
         inicio.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 inicioActionPerformed(evt);
-            }
-        });
-
-        menu.setIcon(new javax.swing.ImageIcon(getClass().getResource("/menu.png"))); // NOI18N
-        menu.setBorder(null);
-        menu.setBorderPainted(false);
-        menu.setContentAreaFilled(false);
-        menu.setFocusPainted(false);
-        menu.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                menuActionPerformed(evt);
             }
         });
 
@@ -104,73 +98,124 @@ public class Perfil extends javax.swing.JFrame {
             .addGroup(barramenuLayout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(inicio)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 48, Short.MAX_VALUE)
+                .addGap(60, 60, 60)
                 .addComponent(amigos)
-                .addGap(51, 51, 51)
+                .addGap(59, 59, 59)
                 .addComponent(perfil)
-                .addGap(46, 46, 46)
+                .addGap(59, 59, 59)
                 .addComponent(notificaciones)
-                .addGap(51, 51, 51)
-                .addComponent(menu)
-                .addContainerGap())
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         barramenuLayout.setVerticalGroup(
             barramenuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(barramenuLayout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(barramenuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(perfil)
-                    .addGroup(barramenuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addComponent(menu, javax.swing.GroupLayout.Alignment.TRAILING)
-                        .addComponent(inicio))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(barramenuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.CENTER)
                     .addComponent(amigos)
-                    .addComponent(notificaciones))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(perfil)
+                    .addComponent(notificaciones)
+                    .addComponent(inicio)))
         );
 
-        infousuario.setBackground(new java.awt.Color(255, 255, 255));
+        jScrollPane2.setBackground(new java.awt.Color(255, 255, 255));
+        jScrollPane2.setBorder(null);
+        jScrollPane2.setForeground(new java.awt.Color(255, 255, 255));
+        jScrollPane2.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 
-        javax.swing.GroupLayout infousuarioLayout = new javax.swing.GroupLayout(infousuario);
-        infousuario.setLayout(infousuarioLayout);
-        infousuarioLayout.setHorizontalGroup(
-            infousuarioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
+        publicaciones.setBackground(new java.awt.Color(255, 255, 255));
+
+        nombreusuario.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        nombreusuario.setText("Nombre Usuario");
+
+        notperfil.setIcon(new javax.swing.ImageIcon(getClass().getResource("/not2.png"))); // NOI18N
+        notperfil.setBorderPainted(false);
+        notperfil.setContentAreaFilled(false);
+        notperfil.setFocusPainted(false);
+
+        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/editarperfil.png"))); // NOI18N
+        jButton1.setBorderPainted(false);
+        jButton1.setContentAreaFilled(false);
+        jButton1.setFocusPainted(false);
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout publicacionesLayout = new javax.swing.GroupLayout(publicaciones);
+        publicaciones.setLayout(publicacionesLayout);
+        publicacionesLayout.setHorizontalGroup(
+            publicacionesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(publicacionesLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(nombreusuario, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(notperfil)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 177, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addGap(18, 18, 18))
         );
-        infousuarioLayout.setVerticalGroup(
-            infousuarioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 100, Short.MAX_VALUE)
+        publicacionesLayout.setVerticalGroup(
+            publicacionesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(publicacionesLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(publicacionesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(publicacionesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(nombreusuario, javax.swing.GroupLayout.PREFERRED_SIZE, 16, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(notperfil, javax.swing.GroupLayout.PREFERRED_SIZE, 16, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jButton1))
+                .addContainerGap(353, Short.MAX_VALUE))
         );
+
+        jScrollPane2.setViewportView(publicaciones);
+
+        menu.setIcon(new javax.swing.ImageIcon(getClass().getResource("/menu.png"))); // NOI18N
+        menu.setBorder(null);
+        menu.setBorderPainted(false);
+        menu.setContentAreaFilled(false);
+        menu.setFocusPainted(false);
+        menu.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                menuActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout backgroundLayout = new javax.swing.GroupLayout(background);
         background.setLayout(backgroundLayout);
         backgroundLayout.setHorizontalGroup(
             backgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(backgroundLayout.createSequentialGroup()
-                .addGroup(backgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(infousuario, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(barramenu, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap(30, Short.MAX_VALUE))
+                .addGroup(backgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(backgroundLayout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jScrollPane2))
+                    .addGroup(backgroundLayout.createSequentialGroup()
+                        .addComponent(barramenu, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(menu)))
+                .addContainerGap())
         );
         backgroundLayout.setVerticalGroup(
             backgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(backgroundLayout.createSequentialGroup()
-                .addComponent(barramenu, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(infousuario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 614, Short.MAX_VALUE))
+                .addGroup(backgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(barramenu, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(menu))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 17, Short.MAX_VALUE)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 390, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
-
-        jScrollPane1.setViewportView(background);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 340, Short.MAX_VALUE)
+            .addComponent(background, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(background, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 6, Short.MAX_VALUE))
         );
 
         pack();
@@ -182,12 +227,6 @@ public class Perfil extends javax.swing.JFrame {
         Notificaciones N = new Notificaciones();
         N.setVisible(true);
     }//GEN-LAST:event_notificacionesActionPerformed
-
-    private void inicioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_inicioActionPerformed
-        this.dispose();
-        InicioF IF = new InicioF();
-        IF.setVisible(true);
-    }//GEN-LAST:event_inicioActionPerformed
 
     private void amigosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_amigosActionPerformed
         this.dispose();
@@ -206,6 +245,16 @@ public class Perfil extends javax.swing.JFrame {
         Menuframe Mf = new Menuframe();
         Mf.setVisible(true);
     }//GEN-LAST:event_menuActionPerformed
+
+    private void inicioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_inicioActionPerformed
+        this.dispose();
+        InicioF IF = new InicioF();
+        IF.setVisible(true);
+    }//GEN-LAST:event_inicioActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -245,11 +294,14 @@ public class Perfil extends javax.swing.JFrame {
     private javax.swing.JButton amigos;
     private javax.swing.JPanel background;
     private javax.swing.JPanel barramenu;
-    private javax.swing.JPanel infousuario;
     private javax.swing.JButton inicio;
-    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JButton menu;
+    private javax.swing.JLabel nombreusuario;
     private javax.swing.JButton notificaciones;
+    private javax.swing.JButton notperfil;
     private javax.swing.JButton perfil;
+    private javax.swing.JPanel publicaciones;
     // End of variables declaration//GEN-END:variables
 }
